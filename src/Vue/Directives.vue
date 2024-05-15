6ddcc4577829d84f192f7fb2973cf9488cdf5021
FerchoApp <lfgalindoq@ufpso.edu.co>
2024-05-15 12:25:12 -0500

    directivas

---
+++ src/Vue/Directives.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {ref} from "vue"
+
+let msg = ref("soy un mensaje")
+let checked = ref(false)
+let data =["hola","mundo","adios"]
+
+//let filtered = data.filter((word) => word.includes("o")) 
+
+let filtered = data.filter(function(word){
+    return word.includes("0")
+})
+</script>
+<template>
+    <input class="border border-red-500" :class="{' border-green-500':checked}" v-model="msg"/>
+     
+    <div :title="msg">
+        {{msg}}
+    </div>
+    
+    <label for="check">Selecione {{ checked }}</label>
+    <input type="checkbox" v-model="checked">
+
+    <ul>
+        <li v-for="(text , index) in filtered" :key="index">
+            {{ text }}
+        </li>
+    </ul>
+</template>
---
+++ src/Vue/TODO.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+const inputTask = ref('')
+
+function addTask(){
+    tasks.value.push(inputTask.value)
+    console.log(tasks)
+}
+
+</script>
+<template>
+    <div class="py-8 px-10 max-w-96 bg-slate-500 rounded shadow mx-auto mt-10">
+
+        <h1 class="text-center text-lg">Lista de tareas</h1>
+
+        <div class="mt-4 mb-2">
+            <input class="w-4/5" type="text" v-model="inputTask">
+            <button @click="addTask" class="w-1/5 bg-green-400 text-white font-bold">+</button>
+
+            <div v-for="(tasks , index) in tasks" :key="index" class="bg-white/60 px-2">
+                {{ tasks }}
+            </div>
+        </div>
+
+    </div>
+</template>
